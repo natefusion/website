fd4cf77b1e4a14969f3f4b93772bbe20fd602627
Nathan Piel <nthnpiel@gmail.com>
2025-03-08 18:06:36 -0500

    clean up nav.js

--- src/nav.js
+++ src/nav.js
@@ -1,15 +1,10 @@
-/* gipity */
 document.addEventListener('DOMContentLoaded', function() {
-    const currentPage = window.location.pathname; // Get the current path
-
-    const navLinks = document.querySelectorAll('nav a'); // Select all links in your navbar
-
+    const currentPage = window.location.pathname;
+    const navLinks = document.querySelectorAll('nav a');
     navLinks.forEach(link => {
-        const linkPath = new URL(link.href).pathname; // Extract the path from the link's href
-
-        const home_page = linkPath == "/index.html" && currentPage == "/";
-        if (home_page || linkPath === currentPage || (linkPath !== "/" && currentPage.startsWith(linkPath + "/"))) {
-            link.classList.add('me'); // Add the 'active' class to the matching link
+        const linkPath = new URL(link.href).pathname;
+        if (linkPath === currentPage || (linkPath !== "/" && currentPage.startsWith(linkPath))) {
+            link.classList.add('me');
         }
     });
 });
